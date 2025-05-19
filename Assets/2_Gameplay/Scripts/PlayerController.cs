e67e0b0efd4aa489ad747c2c22c991c0cc21e626
Emi179lio <ecalvache@uade.edu.ar>
2025-05-19 16:31:30 -0300

    Polish PlayerController readability

--- Assets/2_Gameplay/Scripts/PlayerController.cs
+++ Assets/2_Gameplay/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,66 +8,90 @@ namespace Gameplay
     [RequireComponent(typeof(Character))]
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private InputActionReference moveInput;
-        [SerializeField] private InputActionReference jumpInput;
+        [System.Serializable]
+        class Action
+        {
+            [SerializeField] InputActionReference input;
+            public System.Action<InputAction.CallbackContext> onInput;
+            public System.Action<Collision> onCollisionEnter;
+
+            public void Connect(bool b)
+            {
+                if (!input) return;
+                
+                if (b)
+                {
+                    input.action.started += onInput;
+                    input.action.performed += onInput;
+                    input.action.canceled += onInput;
+                }
+                else
+                {
+                    input.action.started -= onInput;
+                    input.action.performed -= onInput;
+                    input.action.canceled -= onInput;
+                }
+            }
+        }
+        [SerializeField] private Action move;
+        [SerializeField] private Action jump;
         [SerializeField] private float airborneSpeedMultiplier = .5f;
         [SerializeField] private int maxJumps = 2;
+        List<Action> actions;
         private int _currentJumps;
         private Character _character; //Decorator Pattern
         private Coroutine _jumpCoroutine;
 
         private void Awake()
-            => _character = GetComponent<Character>();
+        {
+            _character = GetComponent<Character>();
+            
+            actions = new List<Action>();
+            
+            actions.Add(move);
+            move.onInput += (ctx) =>
+            {
+                var direction = ctx.ReadValue<Vector2>().ToHorizontalPlane();
+                if (_currentJumps > 0)
+                    direction *= airborneSpeedMultiplier;
+                _character?.SetDirection(direction);
+            };
+            
+            actions.Add(jump);
+            jump.onInput += (ctx) =>
+            {
+                if (ctx.canceled || ctx.started) return;
+                if (_currentJumps >= maxJumps) return;
+                if (_jumpCoroutine != null)
+                    StopCoroutine(_jumpCoroutine);
+                _jumpCoroutine = StartCoroutine(_character.Jump());
+                _currentJumps++;
+            };
+            jump.onCollisionEnter += (c) =>
+            {
+                List<ContactPoint> cPoints = new List<ContactPoint>();
+                c.GetContacts(cPoints);
+                for (var index = 0; index < cPoints.Count; index++)
+                    if (Vector3.Angle(cPoints[index].normal, Vector3.up) < 5)
+                        _currentJumps = 0;
+            };
+        }
 
         private void OnEnable()
         {
-            if (moveInput?.action != null)
-            {
-                moveInput.action.started += HandleMoveInput;
-                moveInput.action.performed += HandleMoveInput;
-                moveInput.action.canceled += HandleMoveInput;
-            }
-            if (jumpInput?.action != null)
-                jumpInput.action.performed += HandleJumpInput;
+            for (int i = 0; i < actions.Count; i++)
+                actions[i].Connect(true);
         }
         private void OnDisable()
         {
-            if (moveInput?.action != null)
-            {
-                moveInput.action.performed -= HandleMoveInput;
-                moveInput.action.canceled -= HandleMoveInput;
-            }
-            if (jumpInput?.action != null)
-                jumpInput.action.performed -= HandleJumpInput;
+            for (int i = 0; i < actions.Count; i++)
+                actions[i].Connect(false);
         }
 
-        private void HandleMoveInput(InputAction.CallbackContext ctx)
+        void OnCollisionEnter(Collision other)
         {
-            var direction = ctx.ReadValue<Vector2>().ToHorizontalPlane();
-            if (_currentJumps > 0)
-                direction *= airborneSpeedMultiplier;
-            _character?.SetDirection(direction);
-        }
-
-        private void HandleJumpInput(InputAction.CallbackContext ctx)
-        {
-            if(_currentJumps >= maxJumps) return;
-            RunJumpCoroutine();
-            _currentJumps++;
-        }
-
-        private void RunJumpCoroutine()
-        {
-            if (_jumpCoroutine != null)
-                StopCoroutine(_jumpCoroutine);
-            _jumpCoroutine = StartCoroutine(_character.Jump());
-        }
-
-        private void OnCollisionEnter(Collision other)
-        {
-            foreach (var contact in other.contacts)
-                if (Vector3.Angle(contact.normal, Vector3.up) < 5)
-                    _currentJumps = 0;
+            for (int i = 0; i < actions.Count; i++)
+                actions[i].onCollisionEnter?.Invoke(other);
         }
     }
 }
